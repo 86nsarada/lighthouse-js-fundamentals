661a0e9d6d249ef14d5fad4836142a6e9193e36f
Sarada <sarada@ranjits-mbp.lan>
2021-08-28 22:34:14 -0400

    My first Java Script Program

---
+++ Myfirstprogram.js
@@ -0,0 +1,14 @@
+console.log("Hellow world")
+
+
+const myliteral=()=>{
+    console.log("My first program")
+}
+
+function myliteral2(){
+    console.log("My first program")
+}
+
+myliteral()
+
+myliteral2()
