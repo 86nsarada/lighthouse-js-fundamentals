049c8be19346e14af9b50373e0b8909702e54ef8
Sarada <sarada@ranjits-mbp.lan>
2021-08-29 12:41:09 -0400

    is even number function

--- even.js
+++ even.js
@@ -1,13 +1,18 @@
 const isEven = function(num)
-{
+
    /* if(num%2 === 0){
         console.log("Loopy");
         return true;
     } */
 
-    return (num === 2) ?  "Hi" :"Hello"
+    /*return (num === 2) ?  "Hi" :"Hello"
 
     //return (num%2 === 0) ? true : false
 }
 
-console.log(isEven("2"));
+console.log(isEven("2")); */
+{
+ return num % 2===0;
+}
+console.log(isEven(10));
+console.log(isEven(11));
