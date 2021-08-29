5c62da2469675845d79a441f8d96f28f79f5ae4a
Sarada <sarada@ranjits-mbp.lan>
2021-08-29 12:34:22 -0400

    string and integer comparition

---
+++ even.js
@@ -0,0 +1,13 @@
+const isEven = function(num)
+{
+   /* if(num%2 === 0){
+        console.log("Loopy");
+        return true;
+    } */
+
+    return (num === 2) ?  "Hi" :"Hello"
+
+    //return (num%2 === 0) ? true : false
+}
+
+console.log(isEven("2"));
