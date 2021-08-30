6d553b85ddd4fe659a1d75b80960dd4d623ff2d9
Sarada <sarada@ranjits-mbp.lan>
2021-08-29 22:32:00 -0400

    loopy program

---
+++ Loopylh.js
@@ -0,0 +1,15 @@
+for (let i = 100; i <= 200; i++) {
+    console.log(i)
+    if (i % 3 === 0 && i % 4 === 0) {
+        console.log("LoopyLighthouse");
+    }
+    else if (i % 3 === 0) {
+        console.log("Loppy");
+    }
+    else if (i % 4 === 0) {
+        console.log("LightHouse");
+    }
+}
+
+
+
