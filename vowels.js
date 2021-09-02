c8511b5e17984a5b0e4912641c0fdfe6366e13d2
Sarada <sarada@ranjits-mbp.lan>
2021-09-01 22:55:24 -0400

    finding vowels in an array

---
+++ vowels.js
@@ -0,0 +1,18 @@
+const numberOfVowels = function(data) {
+    // Put your solution here
+    const vow=['a','e','i','o','u']
+    let count=0
+    for(let i=0;i < data.length ; i++)
+    {
+       if(vow.includes(data[i])){
+           count++;
+       }
+    
+    }
+   return count; 
+}
+  
+  console.log(numberOfVowels("orange"));
+  console.log(numberOfVowels("lighthouse labs"));
+  console.log(numberOfVowels("aeiou"));
+  console.log(numberOfVowels("sarada"));
